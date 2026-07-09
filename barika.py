f04f8d0d29e960d0b18806047bb4c713f317d7f3
barikajb <jbbarika@gmail.com>
2026-07-09 14:21:53 -0400

    dre assignment changes(1)

---
+++ barika.py
@@ -0,0 +1,9 @@
+# Student 2: The Subtraction Specialist 
+sub= [12, 4, 2] 
+subsum = (sub[0] - sub[1] - sub[2])
+print("Expected Output: ", subsum)
+
+# Student 3: The Multiplication Master
+multi= [12, 4, 2]
+multisum = (multi[0]*multi[1]*multi[2])
+print("Expected Output: ", multisum)
