e375672d29b670bd4bbd3f93f8b5dfd936335284
J-O-M-A-R <jomarlearn@gmail.com>
2026-07-09 15:25:07 -0400

    jomar upd -addition

--- jomar.py
+++ jomar.py
@@ -1,2 +1,11 @@
-print("Sprite is better then Coke " * 100)
+test_numbers = [45,45]
+total = sum(test_numbers)
+choice = "y"
+if choice == "y":
+    print(total)
+else:
+    print(bye)
 
+print("=" * 16)
+print("== Total:  $", total)
+print("=" * 16)
